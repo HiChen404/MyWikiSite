23cccf39d7ed38b34a9a93696d4651cd75fa6850
Chen <codingme@icloud.com>
2022-05-07 18:54:12 +0800

    style: edit homepage style

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,11 +1,11 @@
-import React from "react";
-import clsx from "clsx";
-import styles from "./HomepageFeatures.module.css";
+import React from 'react'
+import clsx from 'clsx'
+import styles from './HomepageFeatures.module.css'
 
 const FeatureList = [
   {
-    title: "支持 Markdown",
-    Svg: require("../../static/img/undraw_docusaurus_react.svg").default,
+    title: '支持 Markdown',
+    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     // description: (
     //   <>
     //     你只须专注于使用 Markdown/MDX 格式编写文档和文章即可， Docusaurus
@@ -15,13 +15,13 @@ const FeatureList = [
     // ),
   },
   {
-    title: "Easy to Use",
-    Svg: require("../../static/img/undraw_docusaurus_mountain.svg").default,
+    title: 'Easy to Use',
+    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     // description: <>内容可搜索,让你轻松地在文档中找到所需的内容。</>,
   },
   {
-    title: "基于 React 技术构建",
-    Svg: require("../../static/img/undraw_docusaurus_tree.svg").default,
+    title: '基于 React 技术构建',
+    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     // description: (
     //   <>
     //     Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
@@ -29,32 +29,32 @@ const FeatureList = [
     //   </>
     // ),
   },
-];
+]
 
 function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx("col col--4")}>
-      <div className="text--center">
+    <div className={clsx('col col--4')}>
+      <div className='text--center'>
         <Svg className={styles.featureSvg} alt={title} />
       </div>
-      <div className="text--center padding-horiz--md">
+      <div className='text--center padding-horiz--md'>
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
     </div>
-  );
+  )
 }
 
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
-      <div className="container">
-        <div className="row">
+      <div className='container'>
+        <div className='row'>
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
         </div>
       </div>
     </section>
-  );
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
-import React from "react";
-import clsx from "clsx";
-import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import styles from "./index.module.css";
-import HomepageFeatures from "../components/HomepageFeatures";
-import Head from "@docusaurus/Head";
+import React from 'react'
+import clsx from 'clsx'
+import Layout from '@theme/Layout'
+import Link from '@docusaurus/Link'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import styles from './index.module.css'
+import HomepageFeatures from '../components/HomepageFeatures'
+import Head from '@docusaurus/Head'
+import HeroImg from '../../static/img/Hero.jpg'
 
+const svgList = [
+  { title: 'github', Svg: require('../../static/img/github.svg').default, color: 'black' },
+  { title: 'wechat', Svg: require('../../static/img/wechat.svg').default, color: '#64dd17' },
+  { title: 'zhihu', Svg: require('../../static/img/zhihu.svg').default, color: '#2979ff' },
+]
+const Svg = ({ Svg, color, title }) => <Svg className={styles.svg} style={{ fill: color }} />
 // const MySEO = () => (
 //   <Head>
 //     {/* <title>dfsdfsdf</title> */}
@@ -15,38 +22,65 @@ import Head from "@docusaurus/Head";
 // );
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className='container'>
+        <h1 className='hero__title'>{siteConfig.title}</h1>
+        <p className='hero__subtitle'>{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
+          <Link className='button button--secondary button--lg' to='/docs/intro'>
             🖱Click Here!
           </Link>
         </div>
       </div>
     </header>
-  );
+  )
 }
 
+function MyHero() {
+  return (
+    <div className={styles.myHeroContainer}>
+      <div className={styles.leftContainer}>
+        <h1 className={styles.leftContainer_h1}>
+          Always <br /> Ready to Code.
+        </h1>
+        <p className={styles.leftContainer_p}>
+          Spin up fresh, automated dev environments
+          <br />
+          for each task, in the cloud, in seconds.
+        </p>
+        <div className={styles.buttonContainer}>
+          <button className={styles.button}>Click</button>
+          <span className={styles.buttonLeftText}>
+            Open a workspace. <br /> Start from any Git context.
+          </span>
+          <div className={styles.svgContainer}>
+            {svgList.map((item, index) => {
+              return <Svg {...item} key={item.title} />
+            })}
+          </div>
+        </div>
+      </div>
+      <div className={styles.rightContainer}>
+        <img src={HeroImg} alt='HeroImg' />
+      </div>
+    </div>
+  )
+}
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
     <Layout
       // title={`${siteConfig.title}`}
-      title="Home"
-      description="Wiki知识库/vscode/javascript/软件/工具"
-    >
+      title='Home'
+      description='Wiki知识库/vscode/javascript/软件/工具'>
       {/* <MySEO /> */}
-      <HomepageHeader />
+      {/* <HomepageHeader /> */}
       <main>
+        <MyHero />
         <HomepageFeatures />
       </main>
     </Layout>
-  );
+  )
 }
