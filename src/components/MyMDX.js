67528234d2129cdc7aae3a5e99f39155387e4459
ChenChen <codingme+gitKraken@icloud.com>
2022-08-02 11:31:16 +0800

    feat(com): Highlight 组件增加颜色简写形式调用

--- src/components/MyMDX.js
+++ src/components/MyMDX.js
@@ -1,12 +1,22 @@
-import React from "react"
-export function Highlight({ children, color }) {
+import React from 'react'
+
+const COLOR = {
+  yellow: '#feca57',
+  mint: '#25c2a0',
+  blue: '#1877F2',
+}
+
+export function Highlight({ children, color, colorName }) {
+  if (colorName) {
+    color = COLOR[colorName]
+  }
   return (
     <span
       style={{
         backgroundColor: color,
-        borderRadius: "2px",
-        color: "#fff",
-        padding: "0.2rem",
+        borderRadius: '2px',
+        color: '#fff',
+        padding: '0.2rem',
       }}>
       {children}
     </span>
